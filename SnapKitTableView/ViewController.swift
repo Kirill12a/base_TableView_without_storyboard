a738a3338663a4317a3a459054b9b8f4e5487ab4
Kirill12a <kirilldrozd12@gmail.com>
2022-01-08 22:15:21 +0300

    Initial Commit

---
+++ SnapKitTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapKitTableView
+//
+//  Created by Kirill Drozdov on 08.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
